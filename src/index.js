756e34526bb58475b3271907737e7ead70623796
mbron770 <mbronfin.tsd@gmail.com>
2023-05-24 15:40:28 -0400

    add basic functionality to add text and print in task list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+ 
+ 
+  const form=document.body.querySelector("#create-task-form")
+  form.addEventListener("submit", e => {
+    e.preventDefault()
+
+    const formValue = document.querySelector("#new-task-description").value
+    const newTask = document.createElement('li')
+    newTask.textContent = formValue
+    const taskList = document.querySelector("#tasks")
+    taskList.appendChild(newTask)
+    e.target.reset()
+
+
+    //attempt at delete task functionality
+
+    // const deleteButton = document.createElement('button') 
+    // deleteButton.id = "deleteButton"
+    // deleteButton.textContent = "Delete"
+    // taskList.appendChild(deleteButton)
+
+  }) 
+
+})
